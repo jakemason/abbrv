334a94a572eb23f174d145ae22658ee83eb7ea64
Jake Mason <dev.jakemason@gmail.com>
2022-08-15 01:21:39 -0400

    Rename utils

--- utils/build_debug.cmd
+++ utils/build_debug.cmd
@@ -6,5 +6,5 @@ pushd %~dp0
 :: Need to make sure to set Windows environments for x64 first before we call
 :: our cmake commands in cmake_refresh.sh
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cmd.exe /B /C cmake_build_displace.sh
+cmd.exe /B /C cmake_build.sh
 exit
